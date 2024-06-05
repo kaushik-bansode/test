9172d94f6f1692571e996c0d82faccb36a753c3d
kaushik Bansode <167398348+kaushik-bansode@users.noreply.github.com>
2024-06-05 17:52:58 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,13 +2,13 @@
 for i in range(1,10):
     print("first commit")
     
-for i in range(1,10):
-    print("second commit")
+# for i in range(1,10):
+#     print("second commit")
     
-for i in range(1,10):
-    print("third commit")
+# for i in range(1,10):
+#     print("third commit")
     
-for i in range(1,10):
-    print("fourth commit")
+# for i in range(1,10):
+#     print("fourth commit")
 
     
